ac8ff16170d897f4cd4de25987b7c4a2c8bc04d6
Ashok-WGS <ashok@whyglobalservices.com>
2024-02-16 11:45:20 +0530

    loader added

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -7,10 +7,11 @@ import { RiSettings3Fill } from "react-icons/ri";
 import { ChangePasswordInitValues, ChangePasswordSchema } from "../../Validation/changePasswordValidation";
 import { updatePassword } from "../../services/AdminServices";
 import { useFormik } from "formik";
-
+import Loader from "../../utils/loadder";
 
 const NavBar = () => {
   const [showlog, setShowlog] = useState(false);
+  const [loader, setLoader] = useState(false);
   const forms = useFormik({
     initialValues: ChangePasswordInitValues,
     validationSchema: ChangePasswordSchema,
@@ -75,110 +76,112 @@ const NavBar = () => {
   const getName = localStorage.getItem("username")
 
   return (
-    <div
-      className="w-full lg:w-4/5  h-16 flex flex-row justify-end px-4 fixed left-1/5 right-0 top-0
+    <>
+      {loader ? <Loader date={loader} /> : null}
+      <div
+        className="w-full lg:w-4/5  h-16 flex flex-row justify-end px-4 fixed left-1/5 right-0 top-0
 bg-gray-50 z-20 border-b border-stone-200 "
-    >
-      <div className="flex items-center" ref={menuRef}>
-        <img
-          src={profile}
-          className="w-8 h-8 cursor-pointer rounded-full"
-          onClick={() => setShowlog(!showlog)}
-        />
-
-        {showlog && (
-          <>
-            <div className="fixed top-20 right-4 h-fit w-[145px] bg-white flex flex-col rounded-lg shadow border border-gray-50">
-              <div className="flex flex-col p-3 gap-2">
-                <span className="font-semibold  text-gray-800">Account</span>
-                <span className="text-nav-ash font-md capitalize">{getName}</span>
-              </div>
-              <div className="w-full h-px bg-gray-300"></div>
-              <div className="w-full flex flex-col p-2 gap-2">
-                <div className="flex flex-col  justify-between">
-                  {" "}
-                  <span
-                    className="flex flex-row items-center gap-2 cursor-pointer text-nav-ash px-2 py-1 font-md hover:rounded hover:duration-300  hover:bg-first hover:text-white"
-                    onClick={handleAddEvent}
-                  >
-                    <RiSettings3Fill size={18} />Settings
-                  </span>
-                </div>
-
-                <div className="flex flex-col  justify-between">
-                  <span
-                    className="flex flex-row items-center gap-2 cursor-pointer px-2 py-1
-                      hover:bg-first hover:text-white hover:rounded hover:duration-300 font-md"
-                    onClick={logOut}
-                  >
-                    <TbLogout size={18} />
-                    <span>Log out</span>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-      <Modal
-        title="Change Password"
-        open={isModalOpen}
-        onCancel={handleCancel}
-        footer={false}
       >
-        <div className="w-full flex flex-col gap-3 items-center mt-10 h-fit">
-          <input
-            type="password"
-            placeholder="Current password"
-            className={`${forms.errors.oldPassword && forms.touched.oldPassword
-              ? "border-red-500 w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
-              : "w-[90%]  rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
-              }`}
-            name="oldPassword"
-            id="oldPassword"
-            onBlur={forms.handleBlur}
-            value={forms.values.curentpassword}
-            onChange={forms.handleChange}
+        <div className="flex items-center" ref={menuRef}>
+          <img
+            src={profile}
+            className="w-8 h-8 cursor-pointer rounded-full"
+            onClick={() => setShowlog(!showlog)}
           />
-          <input
-            type="password"
-            placeholder="New password"
-            className={`${forms.errors.newPassword && forms.touched.newPassword
-              ? "border-red-500 w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
-              : "w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
-              }`}
-            name="newPassword"
-            id="newPassword"
-            onBlur={forms.handleBlur}
-            value={forms.values.newPassword}
-            onChange={forms.handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Host Name"
-            className={`${forms.errors.confirmPassword && forms.touched.confirmPassword
-              ? "border-red-500 w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
-              : "w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
-              }`}
-            name="confirmPassword"
-            id="confirmPassword"
-            onBlur={forms.handleBlur}
-            value={forms.values.confirmPassword}
-            onChange={forms.handleChange}
-          />
-        </div>
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            onClick={forms.handleSubmit}
-            className="w-28 bg-first rounded-md text-white h-9 hover:bg-second duration-200 shadow-sm shadow-first hover:shadow-second"
-          >
-            Submit
-          </button>
-        </div>
-      </Modal>
-    </div>
 
+          {showlog && (
+            <>
+              <div className="fixed top-20 right-4 h-fit w-[145px] bg-white flex flex-col rounded-lg shadow border border-gray-50">
+                <div className="flex flex-col p-3 gap-2">
+                  <span className="font-semibold  text-gray-800">Account</span>
+                  <span className="text-nav-ash font-md capitalize">{getName}</span>
+                </div>
+                <div className="w-full h-px bg-gray-300"></div>
+                <div className="w-full flex flex-col p-2 gap-2">
+                  <div className="flex flex-col  justify-between">
+                    {" "}
+                    <span
+                      className="flex flex-row items-center gap-2 cursor-pointer text-nav-ash px-2 py-1 font-md hover:rounded hover:duration-300  hover:bg-first hover:text-white"
+                      onClick={handleAddEvent}
+                    >
+                      <RiSettings3Fill size={18} />Settings
+                    </span>
+                  </div>
+
+                  <div className="flex flex-col  justify-between">
+                    <span
+                      className="flex flex-row items-center gap-2 cursor-pointer px-2 py-1
+                      hover:bg-first hover:text-white hover:rounded hover:duration-300 font-md"
+                      onClick={logOut}
+                    >
+                      <TbLogout size={18} />
+                      <span>Log out</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+        <Modal
+          title="Change Password"
+          open={isModalOpen}
+          onCancel={handleCancel}
+          footer={false}
+        >
+          <div className="w-full flex flex-col gap-3 items-center mt-10 h-fit">
+            <input
+              type="password"
+              placeholder="Current password"
+              className={`${forms.errors.oldPassword && forms.touched.oldPassword
+                ? "border-red-500 w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
+                : "w-[90%]  rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
+                }`}
+              name="oldPassword"
+              id="oldPassword"
+              onBlur={forms.handleBlur}
+              value={forms.values.curentpassword}
+              onChange={forms.handleChange}
+            />
+            <input
+              type="password"
+              placeholder="New password"
+              className={`${forms.errors.newPassword && forms.touched.newPassword
+                ? "border-red-500 w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
+                : "w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
+                }`}
+              name="newPassword"
+              id="newPassword"
+              onBlur={forms.handleBlur}
+              value={forms.values.newPassword}
+              onChange={forms.handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              className={`${forms.errors.confirmPassword && forms.touched.confirmPassword
+                ? "border-red-500 w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
+                : "w-[90%] rounded-md p-2 -mt-3 border-2 bg-gray-200 mb-6"
+                }`}
+              name="confirmPassword"
+              id="confirmPassword"
+              onBlur={forms.handleBlur}
+              value={forms.values.confirmPassword}
+              onChange={forms.handleChange}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              onClick={forms.handleSubmit}
+              className="w-28 bg-first rounded-md text-white h-9 hover:bg-second duration-200 shadow-sm shadow-first hover:shadow-second"
+            >
+              Submit
+            </button>
+          </div>
+        </Modal>
+      </div>
+    </>
   );
 };
 
